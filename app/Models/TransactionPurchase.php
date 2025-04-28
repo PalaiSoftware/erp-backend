2154cf8a98abb30e87daedc9a52c06a65d86e037
pooja khandait <poojakhandait2819@gmail.com>
2025-04-28 12:29:07 +0530

    payment-mode-add-integer

--- app/Models/TransactionPurchase.php
+++ app/Models/TransactionPurchase.php
@@ -19,4 +19,14 @@ class TransactionPurchase extends Model
         'created_at',
         'updated_at',
     ];
+    // Payment mode mapping: string to integer
+    public static $paymentModeMap = [
+        'credit_card' => 1,
+        'debit_card' => 2,
+        'cash' => 3,
+        'upi' => 4,
+        'bank_transfer' => 5,
+        'online' => 6,
+        'phonepe' => 7,
+    ];
 }
